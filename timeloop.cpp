96f653736a13bbaf1f5a6c7d812b73df21a6878d
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2024-04-13 01:17:33 +0800

    Create timeloop.cpp

---
+++ timeloop.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x;
+    int c=0;
+    cin >> x;
+    
+    while (x--){
+        ++c;
+        cout << c << " Abracadabra" << endl;
+    }
+    
+}

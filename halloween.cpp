7f86fe2f1e08bff037fbc117da68ad8dd472d209
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2024-04-13 01:49:08 +0800

    Create halloween.cpp

---
+++ halloween.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1,s2;
+    cin >> s1 >> s2;
+    if ((s1=="OCT" && s2 == "31") || (s1=="DEC" && s2 == "25")) {cout << "yup";}
+    else { cout << "nope";}
+    return 0;
+}
